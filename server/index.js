37b20c1035e2183618305243521a12eb556c11e8
Martin <esim_player@hotmail.com>
2022-02-13 12:12:08 +0100

    new change

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+require('dotenv').config();
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use (bodyParser.json({limit:"30mb", extended:true}));
+app.use (bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=> app.listen(PORT, ()=> console.log(`server running : ${PORT}`)))
+    .catch((error)=>console.log (error.message));
+
+// mongoose.set('useFindAndModify', false);
